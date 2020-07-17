4f98b39063c84e0762d3e9ce42db59677a0318c6
Mads Engel Lundt <madslundt@live.dk>
2020-07-17 23:37:40 +0200

    Added diagram and event source stub

---
+++ Infrastructures/EventStore.Middleware/EventStoreContextProviderStub.cs
@@ -0,0 +1,18 @@
+﻿using EventFlow.EntityFramework;
+using Microsoft.EntityFrameworkCore;
+
+namespace EventStore.Middleware
+{
+    public class EventStoreContextProviderStub : IDbContextProvider<EventSourcingDbContext>
+    {
+        public EventSourcingDbContext CreateContext()
+        {
+            var options = new DbContextOptionsBuilder<EventSourcingDbContext>()
+                .UseInMemoryDatabase("For testing")
+                .Options;
+
+            var context = new EventSourcingDbContext(options);
+            return context;
+        }
+    }
+}
